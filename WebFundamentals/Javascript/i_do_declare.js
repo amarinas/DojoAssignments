c99f8a92726c81b99daa3293bb6eb90ba891deac
Ali Marinas <ali.marinas@icloud.com>
2017-01-29 16:00:03 -0800

    installed js in atom and completed i_do_declare.js

---
+++ WebFundamentals/Javascript/i_do_declare.js
@@ -0,0 +1,26 @@
+var num_one = 123;
+var num_two = 123.98;
+var num_three = 23394;
+var num_four = -44444;
+
+console.log("my age is: ", num_one);
+console.log("today i will jump", num_two);
+console.log("the amount of people that came to my party: ", num_three);
+console.log("my favorite number is: ", num_four);
+
+var hello =  "hi there";
+var good_bye = "see ya!";
+var miss = "why me!!!";
+
+console.log("when i walk in the door you say: ", hello);
+console.log("in the morning when i go to work i say", good_bye);
+console.log("when i miss my bus i say:", miss);
+
+var isTrue = true;
+var isFalse = false;
+
+console.log(isTrue);
+console.log(isFalse);
+
+var none;
+console.log(none);
