17bc28f1d14632d07762ca36da6ea083f9a9cbf9
Ali Marinas <ali.marinas@icloud.com>
2017-01-30 11:16:53 -0800

    completed while_you_wait.js

---
+++ WebFundamentals/Javascript/while_you_wait.js
@@ -0,0 +1,12 @@
+var daysUntilMyBirthday = 60;
+for (var i = daysUntilMyBirthday; i >= 0; i--) {
+    if (i >= 30) {
+        console.log(i + " days until my bday. ugh... :(" );
+      }
+    if (i > 0){
+           console.log(i +" days until my bday. its getting close" );
+         }
+    if (i === 0 ){
+           console.log("its my bday");
+}
+}
