3d37251744e2dbc1ada287714b881b4dcff1d495
Ali Marinas <ali.marinas@icloud.com>
2017-02-09 08:52:45 -0800

    edited basic validation for the result page

--- Python/Flask_Fundamentals/basic_validation/server.py
+++ Python/Flask_Fundamentals/basic_validation/server.py
@@ -11,14 +11,18 @@ def survey():
 @app.route('/process', methods=['POST'])
 def process():
   if len(request.form['name']) < 1:
-    flash("Name cannot be empty!") # just pass a string to the flash function
+    flash("Name cannot be empty!")
+    return redirect('/') # just pass a string to the flash function
   elif len(request.form['name']) > 1:
     flash("Success! Your name is {}".format(request.form['name']))
   if len(request.form['textbox']) < 1:
     flash("please enter a comment  {}".format(request.form['textbox']))
   elif len(request.form['textbox']) > 120:
     flash("please reduce word count to 120 characters {}".format(request.form['textbox']))
+
     # just pass a string to the flash function
-  return redirect('/') # either way the application should return to the index and display the message
+
+  return render_template('result.html') # either way the application should return to the index and display the message
+
 
 app.run(debug=True) # run our server
